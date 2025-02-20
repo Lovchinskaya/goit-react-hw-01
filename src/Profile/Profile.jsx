e6d6740c7cab42ba0cce42baa8c554c9f04a9d49
Lovchinskaya <33342414+Lovchinskaya@users.noreply.github.com>
2025-02-20 20:58:09 +0200

    first task ready

--- src/Profile/Profile.jsx
+++ src/Profile/Profile.jsx
@@ -1,30 +1,36 @@
-// export default function Profile () {
-// return (
-//     <div>
-// <div>
-//   <img
-//     src="https://cdn-icons-png.flaticon.com/512/1077/1077012.png"
-//     alt="User avatar"
-//   />
-//   <p>Petra Marica</p>
-//   <p>@pmarica</p>
-//   <p>Salvador, Brasil</p>
-// </div>
+import userData from "../userData.json";
+import css from "./Profile.module.css";
 
-// <ul>
-//   <li>
-//     <span>Followers</span>
-//     <span>1000</span>
-//   </li>
-//   <li>
-//     <span>Views</span>
-//     <span>2000</span>
-//   </li>
-//   <li>
-//     <span>Likes</span>
-//     <span>3000</span>
-//   </li>
-// </ul>
-// </div>
-// );
-// };
+const {username, tag, location, avatar, stats: {followers, views, likes}} = userData;
+
+export default function Profile () {
+return (
+    <div className={css.list}>
+        <div className={css.wrapper}>
+            <img className={css.avatar}
+                src={avatar}
+                alt={username}
+            />
+            <p className={css.title}>{username}</p>
+            <p className={css.subtitle}>@{tag}</p>
+            <p className={css.subtitle}>{location}</p>
+       </div>
+       <div className={css.wrapperlist}>
+       <ul className={css.item}>
+          <li className={css.items}>
+            <span className={css.titleSocial}>Followers</span>
+            <span className={css.numbers}>{followers}</span>
+          </li>
+          <li className={css.items}>
+            <span className={css.titleSocial}>Views</span>
+            <span className={css.numbers}>{views}</span>
+          </li>
+          <li className={css.items}>
+            <span className={css.titleSocial}>Likes</span>
+            <span className={css.numbers}>{likes}</span>
+          </li>
+       </ul>
+       </div>
+    </div>
+);
+};
